1738e2534d52fbc6b29d00fd3e13a264595ae256
Callie Cramer <ccramer@acponline.org>
2023-09-11 19:14:19 -0400

    add in textures, first 3 animation sections

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,8 +18,100 @@ bio.forEach((button) => {
     });
 });
 
-gsap.to('#text-1', {
-    backgroundPosition: '50% 100%',
+let mm = gsap.matchMedia();
+
+//var tl = gsap.timeline();
+gsap.to(".img-bg", {
+    scrollTrigger: {
+        trigger: ".text-screen",
+        scrub: 1
+    },
+    backgroundPosition: "0% 20%"
+});
+
+gsap.to(".intro-video", {
+        scrollTrigger: {
+            trigger: ".intro-video",
+            start: "top top",
+            end: "+=200",
+            scrub: 1,
+            pin: true,
+        },
+        opacity: 0, 
+        start: "100",
+    });  
+
+gsap.from(".logline", {
+    scrollTrigger: {
+        trigger: ".logline",
+        start: "top center",
+        end: "+=200",
+        toggleActions: "restart pause none none", 
+        scrub: 1,
+    },
+    x: "-120%",
+});
+
+gsap.from(".why", {
+    scrollTrigger: {
+        trigger: ".why",
+        start: "top center",
+        end: "+=200",
+        toggleActions: "restart pause none none", 
+        scrub: 1
+    },
+    x: "130%",
+});
+
+gsap.to ("#text-1", {
+    scrollTrigger: {
+        trigger: "#text-1",
+        start: "top top",
+        end: "+=500",
+        pin: true
+    }
+});
+
+
+gsap.from (".poster-text", {
+    scrollTrigger: {
+        start: "center center",
+        trigger: ".poster",
+        scrub: 1
+    },
+    opacity: 0,
+    duration: 2,
+    ease: "power1.inOut",
+    stagger: 1
+});
+
+gsap.to (".poster .overlay", {
+    scrollTrigger: {
+        start: "center center",
+        trigger: ".poster",
+        scrub: 1
+    },
+    opacity: 0.25,
+    duration: 2,
+    ease: "power1.inOut",
+    stagger: 1
+});
+
+mm.add("(min-width: 768px)", () => {
+    gsap.to (".comps", {
+        scrollTrigger: {
+            trigger: ".comps",
+            start: "center center",
+            end: "+=700",
+            pin: true,
+            markers: true
+        }
+    });
+});
+
+
+/*gsap.to('#text-1', {
+    backgroundPosition: '0 -100000%',
     ease: 'none',
     scrollTrigger: {
         trigger: '#text-1',
@@ -38,4 +130,4 @@ gsap.to('#text-2', {
         end: 'bottom top',
         scrub: true
     }
-});
+});*/
