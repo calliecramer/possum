9fa595dcd043db8b831ff59d8d9179e353fd856a
Callie Cramer <ccramer@acponline.org>
2023-11-22 12:37:41 -0500

    updates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,11 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 require('purecss');
 
+addEventListener("load", () => {
+    document.body.classList.remove('no-scroll');
+    document.getElementById("loading").style.display = 'none';
+});
+
 let bio = document.querySelectorAll('.bio-toggle');
 
 bio.forEach((button) => {
@@ -92,18 +97,7 @@ gsap.from(".logline", {
         toggleActions: "restart pause none none", 
         scrub: 1,
     },
-    x: "-120%",
-});
-
-gsap.from(".why", {
-    scrollTrigger: {
-        trigger: ".why",
-        start: "top center",
-        end: "+=200",
-        toggleActions: "restart pause none none", 
-        scrub: 1
-    },
-    x: "130%",
+    opacity: "0"
 });
 
 gsap.to ("#text-1", {
@@ -175,111 +169,9 @@ gsap.from (".arrow", {
     stagger: 2
 });
 
-// Timeline section
-var point = gsap.utils.toArray('.point');
-
-point.forEach((section) => {
-    gsap.from(section, {
-        scrollTrigger: {
-            trigger: section, 
-            start: 'center center'
-        },
-        opacity: 0,
-        rotation: 0,
-        ease: "power1.inOut"
-    });
-});
-
-var line = gsap.utils.toArray('.line');
-
-line.forEach((section) => {
-    gsap.from(section, {
-        scrollTrigger: {
-            trigger: section, 
-            start: 'top center'
-        },
-        opacity: 0,
-        rotation: 0,
-        ease: "power1.inOut"
-    });
-});
-
-var header = gsap.utils.toArray('.header');
-
-header.forEach((section) => {
-    gsap.from(section, {
-        scrollTrigger: {
-            trigger: section, 
-            start: 'center center'
-        },
-        opacity: 0,
-        rotation: 0,
-        ease: "power1.inOut"
-    });
-});
-
-var handwritten = gsap.utils.toArray('.date');
-
-handwritten.forEach((section) => {
-    gsap.from(section, {
-        scrollTrigger: {
-            trigger: section, 
-            start: 'center center'
-        },
-        opacity: 0,
-        ease: "power1.inOut"
-    });
-});
-
-var timelineContent = gsap.utils.toArray('.content');
-
-timelineContent.forEach((section) => {
-    gsap.from(section, {
-        scrollTrigger: {
-            trigger: section, 
-            start: 'center center'
-        },
-        opacity: 0,
-        ease: "power1.inOut"
-    });
-});
-
 // Crew section
 
-// statement and funding text section
-gsap.from(".statement", {
-    scrollTrigger: {
-        trigger: ".statement",
-        start: "top center",
-        end: "+=200",
-        toggleActions: "restart pause none none", 
-        scrub: 1,
-    },
-    x: "-120%",
-});
 
-gsap.from(".funding", {
-    scrollTrigger: {
-        trigger: ".funding",
-        start: "top center",
-        end: "+=200",
-        toggleActions: "restart pause none none", 
-        scrub: 1
-    },
-    x: "130%",
-});
-
-// @todo: fix mobile issues
-mm.add("(min-width: 768px)", () => {
-    gsap.to ("#text-2", {
-        scrollTrigger: {
-            trigger: "#text-2",
-            start: "top top",
-            end: "+=500",
-            pin: true
-        }
-    });
-});
 /*gsap.to('#text-1', {
     backgroundPosition: '0 -100000%',
     ease: 'none',
